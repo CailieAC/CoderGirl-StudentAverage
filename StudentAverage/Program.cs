981ed8caea37e911eee1580b5c6d2b71d8fe7f21
CailieAC <cailiecarlile@gmail.com>
2019-03-24 23:40:04 -0500

    commit1

--- StudentAverage/Program.cs
+++ StudentAverage/Program.cs
@@ -1,17 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace StudentAverage
 {
-    public static class Program
+    public class Program
     {
         public static void Main()
         {
-            foreach (var student in students)
+            Program program = new Program();
+            program.Run();
+            Console.ReadLine();
+        }
+
+        private void Run()
+        {
+            List<Student> studentList = CreateStudentList();
+
+            foreach (var student in studentList)
             {
                 Console.WriteLine($"{student.Name} Average: {student.GetAverage()}");
             }
 
             Console.ReadLine();
+        }
+
+        private List<Student> CreateStudentList()
+        {
+            List<Student> studentList = new List<Student>();
+
+            foreach (string line in File.ReadLines(@"C:\Users\Cailie\Documents\Visual Studio 2017\Projects\CailieAC\CoderGirl-StudentAverage\studentdata.txt"))
+            {
+                Student student = CreateStudent(line);
+                studentList.Add(student);
+            }
+            return studentList;
+        }
+
+        private Student CreateStudent(string line)
+        {
+            Student student = new Student();
+            string[] properties = line.Split(" ");
+            int[] scores = new int[properties.Length - 1];
+            student.Name = properties[0];
+
+            for (int i = 0; i < properties.Length-1; i++)
+            {
+                scores[i] = int.Parse(properties[i+1]);
+            }
+            student.Scores = scores;
+            return student;
         }
     }
 }
--- StudentAverage/Student.cs
+++ StudentAverage/Student.cs
@@ -10,6 +10,19 @@ namespace StudentAverage
 
         public int? GetAverage()
         {
+            int average = 0;
+            int count = 0;
+            int total = 0;
+
+            foreach (int score in Scores)
+            {
+                total = total + score;
+                count++;
+            }
+            average = total / count;
+
+            return average;
+
         }
     }
 }
